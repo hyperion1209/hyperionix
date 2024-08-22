929544a8f7c483afa01b952c9ba97bad87520e4a
Marius Stan <hyperion1209@gmail.com>
2024-08-22 18:38:23 +0200

    Zsh settings

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -10,6 +10,13 @@ in
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
     shellAliases = myAliases;
+    initExtra = ''
+    PROMPT="%{$fg[cyan]%}[%~] $program %{$fg[default]%}
+%B%{$fg[blue]%}[%{$fg[yellow]%}%n%{$fg[red]%}@%{$fg[yellow]%}%m%{$fg[blue]%}] %(?:%{$fg_bold[green]%}➜:%{$fg_bold[red]%}➜)%{$reset_color%}"
+    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+    '';
   };
 }
