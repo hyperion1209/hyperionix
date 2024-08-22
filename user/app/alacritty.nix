bd5daed9480115ce23479b0eaa7f54e6f1fef49f
Marius Stan <hyperion1209@gmail.com>
2024-08-22 17:46:22 +0200

    Test alacritty settings

--- user/app/alacritty.nix
+++ user/app/alacritty.nix
@@ -6,6 +6,13 @@
   ];
   programs.alacritty.enable = true;
   programs.alacritty.settings = {
-    window.opacity = lib.mkForce 0.85;
+    window = {
+      opacity = lib.mkForce 0.85;
+      dynamic_title = true;
+      padding = {
+        x = 2;
+	y = 2;
+      };
+    };
   };
 }
