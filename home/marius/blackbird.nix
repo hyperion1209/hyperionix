156021c2f87b39d014d8c0c21f9e7bc90c71a3dc
hyperion1209 <hyperion1209@gmail.com>
2024-08-30 23:15:17 +0200

    Adding blackbird

---
+++ home/marius/blackbird.nix
@@ -0,0 +1,22 @@
+{ configVars, ... }:
+{
+  imports = [
+    #################### Required Configs ####################
+    common/core # required
+
+    #################### Host-specific Optional Configs ####################
+    common/optional/sops.nix
+    common/optional/alacritty.nix
+    common/optional/browsers/firefox.nix
+    # common/optional/helper-scripts
+
+    # common/optional/desktops
+  ];
+
+  # services.yubikey-touch-detector.enable = true;
+
+  home = {
+    username = configVars.username;
+    homeDirectory = "/home/${configVars.username}";
+  };
+}
